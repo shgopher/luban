f7e6415f656a443873eb2d709ba57eb026298137
shgopher <shgopher@foxmail.com>
2022-05-25 00:56:46 +0800

    feat(all): init

---
+++ .vuepress/config.js
@@ -0,0 +1,133 @@
+module.exports = {
+  // 站点配置
+
+  title: '鲁班 - 系统设计大全',
+  description: '一本关于系统设计基础，容器，rpc，任务编排，网关的书',
+  head: [
+    ['link', {rel: 'shortcut icon', type: "image/x-icon", href: `/favicon.ico`}],
+  ],
+  host: 'localhost',
+  base:'/luban/',
+  port: 8083,
+  dest: '.vuepress/dist',
+  plugins: [
+    ['vuepress-plugin-container',
+      {
+        type: 'right',
+        defaultTitle: ''
+      }
+    ],
+    ['vuepress-plugin-container',
+      {
+        type: 'center',
+        defaultTitle: ''
+      }
+    ],
+    ['vuepress-plugin-container',
+      {
+        type: 'quote',
+        before: info => `<div class="quote"><p class="title">${info}</p>`,
+        after: '</div>'
+      },
+    ],
+    ['vuepress-plugin-container',
+      {
+        type: 'not-print',
+        defaultTitle: ''
+      },
+    ],
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'G-GFKQEFHX3B'
+      }
+    ],
+    [
+      'vuepress-plugin-comment',
+      {
+        choosen: 'gitalk',
+        options: {
+          clientID: '76d83d8a667c6a59d99e',
+          clientSecret: 'e9c55c0da6c294600056819b4ac0f7b4ac775c85',
+          repo: 'luban',
+          owner: 'shgopher',
+          admin: ['shgopher'],
+          labels: ["Gitalk", "Comment"],
+          id: '<%- ("luban" + (frontmatter.to.path || window.location.pathname)).slice(-50) %>',
+          title: '「Comment」<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>',
+          body: '<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>',
+          distractionFreeMode: false,
+          pagerDirection: 'last',
+        }
+      }
+    ],
+    ['@vuepress/back-to-top'],
+    ['@vuepress/nprogress'],
+    'vuepress-plugin-baidu-autopush',
+    ['vuepress-plugin-baidu-tongji-analytics', {
+      key: '45951f610a1fa82985715b79291a8de9'
+    }],
+  ],
+  markdown: {
+    anchor: {permalink: false},
+    toc: {includeLevel: [2, 3]},
+  },
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: 'https://avatars.githubusercontent.com/u/42873232',
+    lastUpdated: '最后更新',
+    smoothScroll: true,
+    editLinks: true,
+    repo: 'https://github.com/shgopher/luban',
+    docsBranch: 'release',
+    editLinkText: '在GitHub中编辑',
+    // 添加导航栏
+    nav: [
+      {
+        text: '首页', link: '/'
+      }, 
+      {
+        text:"系列教程",
+        ariaLabel: 'Menu',
+        items:[
+          {
+            text:"GOFamily 【go语言教程】",
+            link:"https://shgopher.github.io/GOFamily/",
+          },
+          {
+            text:"408  【基础408知识教程】",
+            link:"https://shgopher.github.io/408/",
+          },
+          {
+            text:"luban  【系统设计教程】",
+            link:"https://shgopher.github.io/luban/",
+          },
+          {
+            text:"dingdang  【工具教程】",
+            link:"https://shgopher.github.io/dingdang/",
+          },
+          {
+            text:"god  【给程序员写的书】",
+            link:"https://shgopher.github.io/god/",
+          },
+        ]
+      },
+      {
+        text:'微信公众号',link:'/#wechat.png',
+      },
+      {
+        text:'作者',link:'https://shgopher.github.io/',
+      },
+    ], 
+    sidebar:[   
+      {
+        title: '架构设计',
+        collapsable: false,
+        children: [
+        ],
+      },
+
+    ],
+  },
+}
---
+++ 容器/README.md
@@ -0,0 +1,9 @@
+# 容器
+## 容器技术基础知识
+## docker
+## k8s
+## istio
+## cilium
+## dapr
+## 参考资料
+- 
